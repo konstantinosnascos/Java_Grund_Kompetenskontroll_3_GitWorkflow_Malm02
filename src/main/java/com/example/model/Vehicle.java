564fc6262f4e3c77125e0afef47e6ec0ef4a25ea
Markus Nikolic <markus12nikolic@hotmail.com>
2025-11-03 12:40:55 +0100

    Jag la in alla datatyper som krävs i både booking och vehicle

--- src/main/java/com/example/model/Vehicle.java
+++ src/main/java/com/example/model/Vehicle.java
@@ -1,4 +1,50 @@
 package com.example.model;
 
 public class Vehicle {
+    private String regNum;
+    private String model;
+    private int year;
+
+    // Konstruktor (för att skapa ett nytt fordon)
+    public Vehicle(String regNum, String model, int year) {
+        this.regNum = regNum;
+        this.model = model;
+        this.year = year;
+    }
+
+    // Getters (läsa värden)
+    public String getRegNum() {
+        return regNum;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    // Setters (ändra värden)
+    public void setRegNum(String regNum) {
+        this.regNum = regNum;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    // (Valfritt) Snygg utskrift för debug eller visning
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "regNum='" + regNum + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                '}';
+    }
 }
