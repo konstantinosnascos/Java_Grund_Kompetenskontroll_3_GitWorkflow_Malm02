4a42a2cbb501113fcc4f8f72bf433c31209d50df
Markus Nikolic <markus12nikolic@hotmail.com>
2025-11-04 08:43:54 +0100

    La in priser för årsmodeller, la in status med getter och setter, har följt f7 till f10. Säg till om jag har missat något

--- src/main/java/com/example/model/Vehicle.java
+++ src/main/java/com/example/model/Vehicle.java
@@ -5,40 +5,49 @@ public class Vehicle {
     private String model;
     private int year;
 
-    // Konstruktor (för att skapa ett nytt fordon)
     public Vehicle(String regNum, String model, int year) {
         this.regNum = regNum;
         this.model = model;
         this.year = year;
     }
 
-    // Getters (läsa värden)
     public String getRegNum() {
         return regNum;
+    }
+
+    public void setRegNum(String regNum) {
+        this.regNum = regNum;
     }
 
     public String getModel() {
         return model;
     }
 
-    public int getYear() {
-        return year;
-    }
-
-    // Setters (ändra värden)
-    public void setRegNum(String regNum) {
-        this.regNum = regNum;
-    }
-
     public void setModel(String model) {
         this.model = model;
+    }
+
+    public int getYear() {
+        return year;
     }
 
     public void setYear(int year) {
         this.year = year;
     }
 
-    // (Valfritt) Snygg utskrift för debug eller visning
+
+    public double calculatePrice() {
+        if (year >= 2020) {
+            return 1500.0;
+        } else if (year >= 2015) {
+            return 1200.0;
+        } else if (year >= 2010) {
+            return 900.0;
+        } else {
+            return 700.0;
+        }
+    }
+
     @Override
     public String toString() {
         return "Vehicle{" +
