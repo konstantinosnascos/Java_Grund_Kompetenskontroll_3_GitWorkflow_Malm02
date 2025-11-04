8976289481e4b1939ebb32ead9ac99856809ebe6
Felix Liden <>
2025-11-04 08:35:33 +0100

    made Customer

--- src/main/java/com/example/model/Customer.java
+++ src/main/java/com/example/model/Customer.java
@@ -1,4 +1,31 @@
 package com.example.model;
 
-public class Customer {
+public class Customer
+{
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
